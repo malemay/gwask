e37faeaa6bd55012e0b979dfd4b6654097925178
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2022-03-22 12:03:03 -0400

    Add Roxygen documentation for function parse_fai

--- R/input_parsing.R
+++ R/input_parsing.R
@@ -4,11 +4,27 @@ format_kmer_gwas <- function(sam_df) {
 
 }
 
-# A function that takes a fai index and returns a list of data that contains information about
-# chromosome lengths so it can be used for Manhattan plotting
-# fai_file: the fai index file to parse
-# pattern: a regular expression to use to keep only chromosomes matching a given pattern
-#          if NULL, all chromosomes are kept
+#' Parse a .fai fasta index file
+#'
+#' This function takes a .fai index and returns some data that is useful
+#' for generating Manhattan plots.
+#'
+#' @param fai_file Character of length one. The path to the .fai index file.
+#' @param pattern Character of length one. A regular expression used to select
+#'   the chromosomes or scaffolds to keep. If NULL, all entries in the .fai
+#'   index are kept
+#'
+#' @return A list of length three with the following elements:
+#'   \itemize{
+#'     \item lengths: Numeric. The length of each sequence
+#'     \item start: Numeric. The (cumulative) start position of that 
+#'                  sequence along the genome
+#'     \item label_pos: Numeric. The position to use for plotting chromosome
+#'                      labels in the Manhattan plot.
+#'   }
+#'   All of the vectors in the list are named according to the chromosome/scaffold
+#' @examples
+#' NULL
 parse_fai <- function(fai_file, pattern = NULL) {
 	fai <- read.table(fai_file, header = FALSE, stringsAsFactors = FALSE,
 			  col.names = c("NAME", "LENGTH", "OFFSET", "LINEBASES", "LINEWIDTH"))
